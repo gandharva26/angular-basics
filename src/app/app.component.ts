db3eee0671d9f11560ada2eadddacf7ef10e57e5
Gandharva Deshpande <gandharva.deshpande@gmail.com>
2023-08-19 00:22:17 -0500

    2nd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,22 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'my-app';
   name:string = "Gandharva";
+  submitState:boolean = false;
   image_path:string = "../favicon.ico"
   defaultVal:string = "hello"
+  isLoggedIn:boolean = false;
+  courses:{name:string}[] = [{name:"CS105"}, {name:"CS205"},{name:"CS206"}]
    passValueToComponent(event:any){
     console.log(event?.target.value)
   }
+
+  checkButton(event:any){
+    if(event.target.value === "submitted"){
+      this.submitState = true
+    }
+    else{
+      this.submitState = false
+    }
+  } 
 
 }
